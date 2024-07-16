5b70c3b5c27d17ce3531c5b5e6eba34099bb930b
YADIRA <yadicep@hotmail.com>
2024-07-16 18:21:48 -0400

    V2_YADY_JS_SMOOTH_SCROLL

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ const carousel = new bootstrap.Carousel(myCarouselElement,{
   ride: true,
 });
 
+//Smooth Scroll
+$(document).ready(function(){
+  $("a").on('click', function(event){
+    if (this.hash !== ""){
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      },1000,function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
