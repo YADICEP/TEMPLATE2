987038649cd3d56f63932dbaf1a55b0898493dbb
YADIRA <yadicep@hotmail.com>
2024-07-16 18:24:58 -0400

    V4_YADY_JS_MENSAJE_EMERGENTE

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,22 @@ $(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
 
+//Activar mensaje emergente del botón enviar del formulario de contacto
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+    appendAlert('¡Tu mensaje ha sido enviado! Gracias por contactarnos...', 'success')
+    })
+}
